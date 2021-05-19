1922b7cb06fe650b01a0baf19258891fb9d4bfb6
Raja R <rraja9513@gmail.com>
2021-05-19 16:02:48 +0530

    multeradded

--- routes/categories.js
+++ routes/categories.js
@@ -1,13 +1,15 @@
 const router=require('express').Router();
+const multer=require('multer');
+const upload=multer({dest:'uploads/'});
 let Category=require('../models/category.model');
 router.route('/').get((req, res) => {
   Category.find()
     .then(categories => res.json(categories))
     .catch(err => res.status(400).json('Error: ' + err));
 });
-router.route('/add').post((req,res)=>{
+router.post('/add',upload.single('image'),(req,res,next)=>{
+  console.log(req.file);
     const cname= req.body.cname;
-    const image=req.body.image;
     const  caloriesburnt = req.body. caloriesburnt;
     const newCategory=new Category({
         cname,
