57508f5e05749c98d9763fdf74a57b041f9e3496
Raja R <rraja9513@gmail.com>
2021-05-19 15:46:49 +0530

    multeradded

--- routes/categories.js
+++ routes/categories.js
@@ -1,30 +1,4 @@
 const router=require('express').Router();
-const multer = require('multer');
-const storage = multer.diskStorage({
-    destination: function(req, file, cb) {
-      cb(null, './uploads/');
-    },
-    filename: function(req, file, cb) {
-        cb(null, Date.now() + file.originalname);  
-    }
-  });
-  const fileFilter = (req, file, cb) => {
-    // reject a file
-    if (file.mimetype === 'image/jpg' || file.mimetype === 'image/png') {
-      cb(null, true);
-    } else {
-      cb(null, false);
-    }
-  };
-  
-  const upload = multer({
-    storage: storage,
-    limits: {
-      fileSize: 1024 * 1024 * 100
-    },
-    fileFilter: fileFilter
-  });
-  
 let Category=require('../models/category.model');
 router.route('/').get((req, res) => {
   Category.find()
