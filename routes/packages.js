5640ed93ffaf8201414129e2c89d83cdd5b6cc6f
Raja R <rraja9513@gmail.com>
2021-05-19 17:12:08 +0530

    multeradded

--- routes/packages.js
+++ routes/packages.js
@@ -1,14 +1,38 @@
 const router=require('express').Router();
+const multer=require('multer');
+const storage = multer.diskStorage({
+  destination: function(req, file, cb) {
+    cb(null, './uploads/');
+  },
+  filename: function(req, file, cb) {
+      cb(null, Date.now() + file.originalname);  
+  }
+});
+const fileFilter = (req, file, cb) => {
+  // reject a file
+  if (file.mimetype === 'video/mp4') {
+    cb(null, true);
+  } else {
+    cb(null, false);
+  }
+};
+const upload = multer({
+  storage: storage,
+  limits: {
+    fileSize: 1024 * 1024 * 100
+  },
+  fileFilter: fileFilter
+});
 let Package=require('../models/package.model');
 router.route('/').get((req,res)=>{
 Package.find()
 .then(packages=>res.json(packages))
 .catch(err => res.status(400).json('Error: ' + err));
 });
-router.route('/add').post((req,res)=>{
+router.post('/add',upload.single('video'),(req,res,next)=>{
     const packagename= req.body.packagename;
     const category=req.body.category;
-    const video = req.body.video;
+    const video = req.file.path;
     const price = req.body.price;
     const subscribers = req.body.subscribers;
     const newPackage=new Package({
